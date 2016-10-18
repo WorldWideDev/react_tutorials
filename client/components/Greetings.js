1bb7f222bced9eef792dc41d6753aee7edd98c82
WorldWideDev <dev206@gmail.com>
2016-10-18 14:33:14 -0700

    react router and basic navigation bt pages

--- client/components/Greetings.js
+++ client/components/Greetings.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-export default () => {
-    return (
-        <h1>Hi</h1>
-    )
+// export default () => {
+//     return (
+//         <div className='jumbotron'>
+//             <h1>Hi</h1>
+//         </div>
+//     );
+// }
+
+class Greetings extends React.Component {
+    render(){
+        return (
+            <div className='jumbotron'>
+                <h1>Hi MOM</h1>
+            </div>
+        )
+    }
 }
+
+export default Greetings;
